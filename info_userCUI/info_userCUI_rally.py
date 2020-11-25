bbbc819622a1d17df338d12ffb93f6ed941f8bd5
Shio3001 <taku2000maru@icloud.com>
2020-11-26 01:52:19 +0900

    とりあえず

--- info_userCUI/info_userCUI_rally.py
+++ info_userCUI/info_userCUI_rally.py
@@ -7,7 +7,6 @@ import copy
 
 class Center:
     def __init__(self, set_point, edit_point, printlayer):
-        #self.toset_edit_point_Center = edit_point.Center()
         self.toset_edit_point_Center = edit_point.Center()
         self.toset_set_point_Center = set_point.Center()
         self.printlayer_Center = printlayer.PrintLayer()
