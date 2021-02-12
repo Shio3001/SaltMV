7d737144a3808eb94b7ea7b458bde4e74181dad6
Shio3001 <taku2000maru@icloud.com>
2021-02-12 11:11:08 +0900

    取得関数の整備

--- plugin/other/UI_data.py
+++ plugin/other/UI_data.py
@@ -15,21 +15,71 @@ class SendUIData:  # パーツひとつあたりのためのclass
         self.common_control = CommonControl(self.window)
         self.tk = tk
 
+        self.canvas_data = CanvasData(self.window)
+
+    def edit_canvas_size(self, name, x=None, y=None):
+        self.canvas_data.size = self.common_control.xy_compilation(self.canvas_data.size, x=x, y=y)
+
+    def edit_canvas_position(self, name, x=None, y=None):
+        self.canvas_data.position = self.common_control.xy_compilation(self.canvas_data.position, x=x, y=y)
+
+    def get_canvas_contact(self):
+        canvas_edge, canvas_join = self.common_control.xy_compilation(self.canvas_data)
+
+        return canvas_edge, canvas_join
+
+    # territory
+
+    def new_territory(self, name):
+        self.canvas_data.territory[name] = TerritoryData(self.canvas_data.canvas)
+
+    def del_territory(self, name):
+        del self.canvas_data.territory[name]
+
+    def edit_territory_size(self, name, x=None, y=None):
+        self.canvas_data.territory[name].size = self.common_control.xy_compilation(self.canvas_data.territory[name].size, x=x, y=y)
+
+    def edit_territory_position(self, name, x=None, y=None):
+        self.canvas_data.territory[name].position = self.common_control.xy_compilation(self.canvas_data.territory[name].position, x=x, y=y)
+
+    def get_territory_contact(self, name):
+        territory_edge, territory_join = self.common_control.xy_compilation(self.canvas_data.territory[name])
+
+        return territory_edge, territory_join
+
+    # diagram
+
+    def new_diagram(self, ca_name, di_name):
+        self.canvas_data.territory[ca_name].diagram[di_name] = DiagramData(self.canvas_data.canvas)
+
+    def del_diagram(self, ca_name, di_name):
+        del self.canvas_data.territory[ca_name].diagram[di_name]
+
+    def new_textbox(self, ca_name, di_name):
+        self.canvas_data.territory[ca_name].diagram[di_name] = TextBoxData(self.canvas_data.canvas)
+
+    def edit_diagram_size(self, ca_name, di_name, x=None, y=None):
+        self.canvas_data.territory[ca_name].diagram[di_name].size = self.common_control.xy_compilation(self.canvas_data.territory[ca_name].diagram[di_name].size, x=x, y=y)
+
+    def edit_diagram_position(self, ca_name, di_name, x=None, y=None):
+        self.canvas_data.territory[ca_name].diagram[di_name].position = self.common_control.xy_compilation(self.canvas_data.territory[ca_name].diagram[di_name].position, x=x, y=y)
+
+    def get_diagram_contact(self, ca_name, di_name):
+        diagram_edge, diagram_join = self.common_control.xy_compilation(self.canvas_data.territory[ca_name].diagram[di_name])
+        return diagram_edge, diagram_join
+
 
 class CanvasData:
     def __init__(self, window):
         self.size = [0, 0]
         self.position = [0, 0]
         self.canvas = tk.Canvas(window, highlightthickness=0, width=self.size[0], height=self.size[1])
+        self.territory = {}
+
+        self.event = {}
 
     def event_link(self):
         pass
-
-
-class TextBoxData:
-    def __init__(self, window):
-        self.position = [0, 0]
-        self.text = ""
 
 
 class TerritoryData:
@@ -37,32 +87,48 @@ class TerritoryData:
         self.size = [0, 0]
         self.position = [0, 0]
 
+        self.canvas = canvas
+
+        self.diagram = {}
+
+        self.event = {}
+
 
 class DiagramData:
     def __init__(self, territory):
-        self.size = []
+        self.size = [0, 0]
         self.position = [0, 0]
         self.text = ""
 
+        self.event = {}
+
     def event_link(self):
         pass
+
+
+class TextBoxData:
+    def __init__(self, territory):
+        self.position = [0, 0]
+        self.text = ""
 
 
 class CommonControl:
     def __init__(self, window):
         self.window = window
 
-    def xy_compilation(self, origin, user_select):
-        for o, s in zip(origin, user_select):
-            if not s is None:
-                o = s
+    def xy_compilation(self, origin, x=None, y=None):  # 設定項目を変更する
+        if not x is None:
+            origin[0] = x
+
+        if not y is None:
+            origin[1] = y
 
         return origin
 
-    def contact_edge(self):
-        return
+    # def contact_edge(self):
+    #    return
 
-    def event_temporary(self):
+    def event_bind(self):
         return
 
     def get_mouse_position(self):  # マウスの位置を取得
@@ -90,7 +156,7 @@ class CommonControl:
             if (data.position[i] - permission) <= mouse[i] <= (data.position[i] + data.size[i] + permission):
                 join_detection[i] = True
 
-            if join_detection[0] and join_detection[1]:
-                join_detection[2] = True
+        if join_detection[0] and join_detection[1]:
+            join_detection[2] = True
 
         return edge_detection, join_detection
