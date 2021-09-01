6b2c0658fd248c2c945e4eb06c0facc8cfaf8c85
Shio3001 <taku2000maru@icloud.com>
2021-09-01 14:45:50 +0900

    image修正

--- pysrc/plugin/effect/image.py
+++ pysrc/plugin/effect/image.py
@@ -18,14 +18,28 @@ class CentralRole:
     def __init__(self):
         self.starting_point = [0, 0]
 
+        self.image_data = None
+        self.now_file = None
+        self.open_status = False
+
+    def setup(self, file_name):
+        try:
+            image_dataBGR = cv2.imread(file_name)
+            self.image_data = cv2.cvtColor(image_dataBGR, cv2.COLOR_BGR2RGBA)
+            self.now_file = copy.deepcopy(file_name)
+            self.open_status = True
+
+        except:
+            self.open_status = False
+
     def main(self, data):
 
         path = data.various_fixed["path"]
 
-        try:
-            im = cv2.imread(path)
-            im = cv2.cvtColor(im, cv2.COLOR_BGR2RGBA)
-        except:
-            return data.draw, self.starting_point
+        if path != self.now_file or not self.open_status:
+            self.setup(path)
 
-        return im, self.starting_point
+        if not self.open_status:
+            self.image_data = data.draw
+
+        return self.image_data, self.starting_point
