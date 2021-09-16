f0e270841305c0b1629f30f11a1070e33c51b11a
Shio3001 <taku2000maru@icloud.com>
2021-09-16 17:28:22 +0900

    nannde

--- pysrc/Internal_operation/rendering_py/rendering_main.py
+++ pysrc/Internal_operation/rendering_py/rendering_main.py
@@ -245,8 +245,8 @@ class SceneOutput:
         if run:
             self.audio_control.sound_run(frame)
 
-        object_group = self.cpp_encode.object_group_recovery()
-        self.get_set_media_object_group(data=object_group,copy_obj=False)
+        #object_group = self.cpp_encode.object_group_recovery()
+        # self.get_set_media_object_group(data=object_group)
 
         if self.preview == "opencv":
             #resize_size_opencv = (640, 360)
