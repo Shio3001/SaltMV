4e9ef325b0e8cef522d7e91dfdcca68eb923cf32
Shio3001 <taku2000maru@icloud.com>
2021-11-18 16:00:32 +0900

    にゃ＾ん

---
+++ pysrc/plugin/expansion/easing.py
@@ -0,0 +1,20 @@
+# coding:utf-8
+import sys
+import os
+import copy
+import datetime
+import inspect
+import time
+
+
+class InitialValue:
+    def __init__(self, window_control):
+        self.window_control = window_control
+        self.operation = self.window_control.operation
+
+    def main(self):
+        pass
+
+
+class CentralRole:
+    pass
